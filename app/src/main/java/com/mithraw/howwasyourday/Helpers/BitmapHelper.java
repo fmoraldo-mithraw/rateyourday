f9eb183c87e990bf4491c3f3350b185ddcf8dec1
fmoraldo-mithraw <mithraw@gmail.com>
2018-04-21 04:57:39 +0200

    Fix the repository name of the images with correct dates (forced to two digits)

--- app/src/main/java/com/mithraw/howwasyourday/Helpers/BitmapHelper.java
+++ app/src/main/java/com/mithraw/howwasyourday/Helpers/BitmapHelper.java
@@ -157,7 +157,7 @@ public class BitmapHelper {
     }
 
     public static String getDayImageDir(Calendar cal) {
-        String dir = getImagesDir() + cal.get(Calendar.YEAR) + cal.get(Calendar.MONTH) + cal.get(Calendar.DAY_OF_MONTH);
+        String dir = getImagesDir() + cal.get(Calendar.YEAR) + String.format("%02d",cal.get(Calendar.MONTH)) + String.format("%02d",cal.get(Calendar.DAY_OF_MONTH));
         File file = new File(dir);
         file.mkdirs();
         return dir;
