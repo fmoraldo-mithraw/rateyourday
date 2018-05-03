26deacd22215fd7624c0ab76b1d73fc6ee56944f
fmoraldo-mithraw <mithraw@gmail.com>
2018-05-03 15:28:22 +0200

    If no location service or no presaved location, we get the last location from last day

--- app/src/main/java/com/mithraw/howwasyourday/Activities/MapPickActivity.java
+++ app/src/main/java/com/mithraw/howwasyourday/Activities/MapPickActivity.java
@@ -1,9 +1,12 @@
 package com.mithraw.howwasyourday.Activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -19,14 +22,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mithraw.howwasyourday.R;
+import com.mithraw.howwasyourday.Tools.Coordinate;
 import com.mithraw.howwasyourday.Tools.Map.IconManager;
 import com.mithraw.howwasyourday.Tools.MyLocationManager;
+import com.mithraw.howwasyourday.databases.Day;
+import com.mithraw.howwasyourday.databases.DaysDatabase;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 public class MapPickActivity extends AppCompatActivity implements OnMapReadyCallback {
     public final static String EXTRA_PARAM_RATE = "ExpandedDayActivity_Rate";
     public final static String EXTRA_PARAM_TITLE = "ExpandedDayActivity_Title";
     public final static String EXTRA_PARAM_LONGITUDE = "ExpandedDayActivity_LONGITUDE";
     public final static String EXTRA_PARAM_LATITUDE = "ExpandedDayActivity_LATITUDE";
+
+    private enum MSG_ID {LAST_LOCATION}
     MyLocationManager mLocManager;
     GoogleMap map;
     MapView mMapView;
@@ -35,8 +47,10 @@ public class MapPickActivity extends AppCompatActivity implements OnMapReadyCall
     double mLongitude = 0;
     int mRate = 0;
     boolean hasMovedOnce = false;
+    Handler handler;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +77,7 @@ public class MapPickActivity extends AppCompatActivity implements OnMapReadyCall
             public void onPlaceSelected(Place place) {
                 pickLocation(place.getLatLng());
             }
+
             @Override
             public void onError(Status status) {
             }
@@ -103,8 +118,25 @@ public class MapPickActivity extends AppCompatActivity implements OnMapReadyCall
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
         mMapView.onCreate(mapViewBundle);
-
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MSG_ID.LAST_LOCATION.ordinal()) {
+                    Coordinate coord = (Coordinate) msg.obj;
+                    if ((coord != null) && (map != null) && (!hasMovedOnce)) {
+                        LatLng latLng = new LatLng(coord.getLatitude(), coord.getLongitude());
+                        map.clear();
+                        map.addMarker(new MarkerOptions().position(latLng).title(mTitle).icon(IconManager.getInstance().getIcon(mRate)));
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+                        map.animateCamera(cameraUpdate);
+                        mLatitude = latLng.latitude;
+                        mLongitude = latLng.longitude;
+                    }
+                }
+            }
+        };
         mMapView.getMapAsync(this);
+
 
     }
 
@@ -154,6 +186,26 @@ public class MapPickActivity extends AppCompatActivity implements OnMapReadyCall
             mLongitude = latLng.longitude;
         }
     }
+
+    public void getDbLastLocation() {
+        new Thread() {
+            @Override
+            public void run() {
+                DaysDatabase db = DaysDatabase.getInstance(getBaseContext());
+                //Last year
+                List<Day> days = db.dayDao().getLastOneWithCoordinate();
+                if(days.size() >0){
+                    Coordinate coord = new Coordinate();
+                    coord.setLatitude(days.get(0).getLatitude());
+                    coord.setLongitude(days.get(0).getLongitude());
+                    Message msg = handler.obtainMessage();
+                    msg.what = MSG_ID.LAST_LOCATION.ordinal();
+                    msg.obj = coord;
+                    handler.sendMessage(msg);
+                }
+            }
+        }.start();
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
@@ -174,6 +226,8 @@ public class MapPickActivity extends AppCompatActivity implements OnMapReadyCall
                 mLatitude = latLng.latitude;
                 mLongitude = latLng.longitude;
                 hasMovedOnce = true;
+            } else if ((loc == null) && (!hasMovedOnce)) {
+                getDbLastLocation();
             }
         } else {
             LatLng latLng = new LatLng(mLatitude, mLongitude);
